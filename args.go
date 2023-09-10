c0c41006ce7cd46d00892ccd6cacf2617c4692ae
Tomkoid <tomkoid@tomkoid.tk>
2023-09-10 14:00:13 +0200

    watch: add full support for watching files and automatically reconvert them

--- args.go
+++ args.go
@@ -12,6 +12,7 @@ type Args struct {
 	file  string
 	out   string
 	style string
+	watch bool
 }
 
 func parseArgs() Args {
@@ -19,6 +20,7 @@ func parseArgs() Args {
 	file := flag.StringP("file", "f", "", "The markdown file to convert to HTML")
 	out := flag.StringP("out", "o", "", "The destination file to write the HTML to")
 	style := flag.StringP("style", "s", "", "Apply extra styling to the HTML using a CSS file")
+	watch := flag.BoolP("watch", "w", false, "Watch the source file for changes and reconvert when changes are detected")
 
 	flag.Parse()
 
@@ -37,5 +39,6 @@ func parseArgs() Args {
 		file:  *file,
 		out:   *out,
 		style: *style,
+		watch: *watch,
 	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"os"
@@ -58,9 +60,58 @@ func transform(args Args, debug bool) {
 	}
 }
 
+func shaString(s string) string {
+	h := sha1.New()
+	h.Write([]byte(s))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func transformWatch(args Args, debug bool) {
+	// get initial content
+	content, err := os.ReadFile(args.file)
+	if err != nil {
+		log.Fatalf("Error reading file: %s", err)
+		os.Exit(1)
+	}
+
+	// get the hash
+	oldHash := shaString(string(content))
+
+	for {
+		// get the new hash
+		content, err := os.ReadFile(args.file)
+		if err != nil {
+			log.Fatalf("Error reading file: %s", err)
+			os.Exit(1)
+		}
+
+		newHash := shaString(string(content))
+
+		// compare the hashes
+		if oldHash != newHash {
+			fmt.Println("== Detected change in file, transforming...")
+
+			// if they're different, transform the file
+			transform(args, false)
+
+			// update the old hash
+			oldHash = newHash
+		}
+
+		// sleep for 500ms
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
 func main() {
 	// parse arguments
 	args := parseArgs()
+
+	if args.watch {
+		fmt.Printf("== Watching %s for changes...\n", args.file)
+		transformWatch(args, true)
+		return
+	}
 
 	checkFilesExist(args)
 
