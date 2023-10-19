5edd822d4bfa55999b8af76986c944d64ae0c1d5
Tomkoid <tomkoid@tomkoid.tk>
2023-10-19 17:00:57 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "mdhtml flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = (import ./default.nix { inherit pkgs; });
+    };
+}
