32a1c09b91941678dc920ef1cf28f41b1e81c31b
Tomkoid <tomkoid@tomkoid.tk>
2023-11-03 20:19:16 +0100

    reload.js: auto reconnect if connection lost
    Signed-off-by: Tomkoid <tomkoid@tomkoid.tk>

--- internal/httpserver/scripts/reload.js
+++ internal/httpserver/scripts/reload.js
@@ -1,45 +1,59 @@
-var loc = window.location;
-var uri = 'ws:';
+function connect() {
+  var loc = window.location;
+  var uri = 'ws:';
 
-if (loc.protocol === 'https:') {
-  uri = 'wss:';
-}
-uri += '//' + loc.host;
-uri += loc.pathname + 'ws';
-
-const socket = new WebSocket(uri);
-
-console.log('[websocket] connecting...')
-
-socket.onopen = () => {
-  console.log('[websocket] connected');
-  socket.send('connected');
-
-}
-
-socket.onmessage = async (event) => {
-  if (event.data === 'hello') {
-    console.log('[websocket] server responded!');
-    return
+  if (loc.protocol === 'https:') {
+    uri = 'wss:';
   }
-  console.log(`[websocket] message: ${event.data}`);
-  if (event.data === 'reload') {
-    const content = await fetch('/');
-    const html = await content.text();
+  uri += '//' + loc.host;
+  uri += loc.pathname + 'ws';
 
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
+  const socket = new WebSocket(uri);
 
-    // use the new body instead of the old one
-    document.body = doc.body;
+  console.log('[websocket] connecting...')
+
+  socket.onopen = () => {
+    console.log('[websocket] connected');
+    socket.send('connected');
+
+  }
+
+  socket.onmessage = async (event) => {
+    if (event.data === 'hello') {
+      console.log('[websocket] server responded!');
+      return
+    }
+    console.log(`[websocket] message: ${event.data}`);
+    if (event.data === 'reload') {
+      const content = await fetch('/');
+      const html = await content.text();
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+
+      // use the new body instead of the old one
+      document.body = doc.body;
+    }
+  }
+
+  socket.onclose = () => {
+    console.log('[websocket] disconnected');
+    console.log("reconnecting...")
+
+    socket.close();
+
+    setTimeout(() => {
+      connect();
+    }, 1000);
+  }
+
+  socket.onerror = (error) => {
+    console.log(`[websocket] error: ${error}`);
+    console.log(error)
+
+    // this will go to the onclose function
+    socket.close();
   }
 }
 
-socket.onclose = () => {
-  console.log('[websocket] disconnected');
-}
-
-socket.onerror = (error) => {
-  console.log(`[websocket] error: ${error}`);
-  console.log(error)
-}
+connect();
