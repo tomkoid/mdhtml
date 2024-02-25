3a7a3fc697fffe246213e926d6276a07b88e5d9e
Tomkoid <tomaszierl@outlook.com>
2024-02-25 12:01:09 +0100

    nix(dev shell): add rust-overlay and add proper buildInputs
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- flake.nix
+++ flake.nix
@@ -4,20 +4,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system; inherit overlays; };
       in
       {
         defaultPackage = (import ./default.nix { inherit pkgs; });
         devShells.default = pkgs.mkShell
           {
             buildInputs = with pkgs; [
-              go
-              golangci-lint
+              rust-bin.stable.latest.default
             ];
           };
       }
