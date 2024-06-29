0b32dd026b3ef41f4fce97ccc698c71ecca83d12
Tomkoid <tomaszierl@outlook.com>
2024-06-29 12:26:04 +0200

    args: add help to arguments
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/args.rs
+++ src/args.rs
@@ -14,19 +14,35 @@ pub enum Commands {
 
 #[derive(Parser, Default, Clone, Debug)]
 pub struct Convert {
+    /// Input file 
     #[clap(required = true)]
     pub input: String,
+
+    /// Output file
     #[clap(short, long, default_value = "[INPUT].html")]
     pub output: String,
+
+    /// Path to a CSS file to use in the HTML
     #[clap(short, long)]
     pub style: Option<String>,
+
+    /// Disables things like highlight.js
     #[clap(short, long, default_value_t = false)]
     pub no_external_libs: bool,
+
+    /// Convert without any additional JavaScript
     #[clap(long)]
     pub raw: bool,
 
+    /// Open in web browser after finishing converting or starting server
+    #[clap(short = 'O', long, default_value_t = false)]
+    pub open: bool,
+
+    /// Start a HTTP server to serve the HTML file and reload the page when changes are detected
     #[clap(long)]
     pub server: bool,
+
+    /// Prints additional debug info 
     #[clap(long)]
     pub debug: bool,
 }
